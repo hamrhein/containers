3f9e5f0e5845c6f23ba9aa2d964cb7c33bf987a0
Henry Amrhein <hamrhein@caltech.edu>
2023-01-24 15:56:11 -0800

    Administration
    HA - Initial check-in

---
+++ apache_gunicorn_flask/app.py
@@ -0,0 +1,11 @@
+from flask import Flask 
+
+app = Flask(__name__)
+
+
+@app.route('/test')
+def get_test():
+    return 'Success' 
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
---
+++ apache_gunicorn_flask/gunicorn_config.py
@@ -0,0 +1,5 @@
+workers = 8
+bind = 'unix:/app/gunicorn.sock'
+reload = True
+accesslog = '-'
+errorlog = '-'
